9d2d0a5b9dcbe89cf4a572d80b2e2f47f1836e48
veluaru <angie.castaneda@udea.edu.co>
2020-01-24 16:21:58 -0500

    Primer version buscar objeto

--- src/app/buscarobjeto/buscarobjeto.component.ts
+++ src/app/buscarobjeto/buscarobjeto.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-buscarObjeto',
-  templateUrl: './buscarObjeto.component.html',
-  styleUrls: ['./buscarObjeto.component.css']
+  selector: 'app-buscarobjeto',
+  templateUrl: './buscarobjeto.component.html',
+  styleUrls: ['./buscarobjeto.component.css']
 })
 export class BuscarObjetoComponent implements OnInit {
+
+  cosita=["a","b","c"];
 
   constructor() { }
 
