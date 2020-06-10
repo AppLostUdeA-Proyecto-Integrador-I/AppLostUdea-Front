4390f780dacb98fcf78ef719d2b5dbc3325f8828
Okabe94 <pipe9411@hotmail.com>
2020-06-09 19:36:34 -0500

    Gráfica de barras completa e información corregida en las demás

--- src/app/estadisticas/estadisticas.component.ts
+++ src/app/estadisticas/estadisticas.component.ts
@@ -14,10 +14,10 @@ export class EstadisticasComponent implements OnInit {
 
   // Llevar a otro lado independiente
   colorList = [
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(255, 206, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
+    'rgba(255, 99, 132, 0.3)',
+    'rgba(54, 162, 235, 0.3)',
+    'rgba(255, 206, 86, 0.3)',
+    'rgba(75, 192, 192, 0.3)',
   ]
 
   BAR_CHART_OPTIONS = {
@@ -30,114 +30,64 @@ export class EstadisticasComponent implements OnInit {
     }
   }
 
-  // Función que añadirá colores al arreglo dependiendo de la carga de 
-  // contenido que tenga la gráfica
-
-  /* 
-  Pasos:
-   * Traer los datos (OK)
-   * Crear los arreglos correspondientes (OK) 
-   * Gráficas:
-      - Pie: Cantidad vs Categoría (OK)
-      - Barras: Cantidad vs Mes
-   * Arreglo de colores correspondiente al número de objetos en la gráfica
-   * Entregar las gráficas
-   * Pintar (OK)
-   * Refactor
-  */
-  
-  listaCategorias = []
-  array = []
-
   constructor(public api: ApiServiceService) { }
 
   ngOnInit() {
     const allObjects = this.api.getObjects()
     const allCategories = this.api.getCategories()
 
-    var categoryAmount = [], uniqueCategoryId = [], categoryLabels = []
+    var statusAmount = [], uniqueStatus = []
+    var categoryAmount = [], uniqueCategory = [], categoryLabels = []
+    const monthsLabels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+    var monthsAmount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+    // Sincroniza las respuestas de ambas promesas para ejecutar una vez terminen
     combineLatest(allObjects, allCategories, (objects, categories) => ({objects, categories}))
     .subscribe(pair => {
+
       console.log("Objects: ", pair.objects)
       console.log("Categories: ", pair.categories)
+      
+      var currentYear = new Date().getFullYear()
 
       // Creamos un arreglo que toma todos los objetos
       // Y separa cada categoría
       pair.objects.forEach(element => {
+        
         // Si el elemento ya existe en el discriminado de categorías
         // Se busca la posición en la que se almacena la cantidad y se suma 1
-        if (uniqueCategoryId.includes(element.categoriasId)) {
-          categoryAmount[uniqueCategoryId.indexOf(element.categoriasId)] += 1
+        if (uniqueStatus.includes(element.estado)) {
+          statusAmount[uniqueStatus.indexOf(element.estado)] += 1
         } else {
           // De lo contrario, se crea una nueva entrada y se inicia en 1
-          uniqueCategoryId.push(element.categoriasId)
+          uniqueStatus.push(element.estado)
+          statusAmount.push(1)
+        }
+
+        if (uniqueCategory.includes(element.categoriasId)) {
+          categoryAmount[uniqueCategory.indexOf(element.categoriasId)] += 1
+        } else {
+          uniqueCategory.push(element.categoriasId)
           categoryLabels.push(this.getCategoryNameById(element.categoriasId, pair.categories))
           categoryAmount.push(1)
         }
+
+        var dateSplit = element.fechaEncontrado.split('-', 3)
+        if (dateSplit[0] == currentYear)
+          monthsAmount[parseInt(dateSplit[1]) - 1] += 1
       });
 
       // Creamos la gráfica de dona
-      this.createChart("pieChart", "doughnut", categoryLabels, "Cantidad por Categoría", categoryAmount, this.colorList)
+      this.createChart("pieChart", "doughnut", uniqueStatus, "Cantidad por Estado", statusAmount, this.createColors(statusAmount))
+
+      // Creamos la gráfica de barras
+      this.createChart("barChart", "bar", categoryLabels, "Cantidad por Categoría", categoryAmount, this.createColors(categoryAmount), true, this.BAR_CHART_OPTIONS)
+
+      
+      // Creamos la gráfica de lineas
+      this.createChart("lineChart", "line", monthsLabels, "Cantidad por Mes", monthsAmount, this.createColors(monthsAmount), false)
     })
     
-
-    // this.api.getObjects().subscribe((response: any) => {
-    //   for (var item of response) {
-    //     this.listaCategorias.push(item.nombre);
-    //   }
-    //   var myChart = new Chart("myChart", {
-    //     type: 'doughnut',
-    //     data: {
-    //       labels: this.listaCategorias,
-    //       datasets: [{
-    //         label: '# of Votes',
-    //         data: [12, 19, 3, 5, 2, 3],
-    //         backgroundColor: this.colorList,
-    //         borderWidth: 1
-    //       }],
-    //     },
-    //   });
-    //   console.log(response)
-    // }, error => console.error(error));
-
-    var myChart = new Chart("myChart2", {
-      type: 'bar',
-      data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        },
-      ]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
   }
 
   getCategoryNameById(id, categories) {
@@ -150,7 +100,7 @@ export class EstadisticasComponent implements OnInit {
 
   // Crea la tabla apartir de los parámetros entregados
   createChart(canvasId: any, chartType: String, labelList: String[], 
-    chartLabel: String, data: Number[], colorList: String[], options: {} = {}) {
+    chartLabel: String, data: Number[], colorList: String[], fill: boolean = true, options: {} = {}): Chart {
     return new Chart(canvasId, {
       type: chartType,
       data: {
@@ -160,11 +110,24 @@ export class EstadisticasComponent implements OnInit {
           data: data,
           backgroundColor: colorList,
           borderColor: colorList,
-          borderWidth: 1
+          borderWidth: 3,
+          fill: fill
         }]
       },
       options: options
     });
+  }
+
+  createColors(data: Number[]){
+    var colors = []
+    var count = 0
+    for (var i = 0; i < data.length; i++) {
+      if (count % this.colorList.length == 0)
+        count = 0    
+      colors.push(this.colorList[count++])
+    }
+    console.log(colors)
+    return colors
   }
 
 }
